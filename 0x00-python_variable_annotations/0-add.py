ea52aa647d6acd9856a6b60b36ff07ed79cb66d0
Barhamou <hama.peinture.barhamou@gmail.com>
2023-01-05 15:14:29 +0100

    0. Basic annotations - add comment

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-"""
-    function add that takes a float a and a float b as arguments
-    and returns their sum as a float
-"""
+""" module for annotation work python"""
 
 def add(a: float, b: float) -> float:
+    """
+        function add that takes a float a and a float b as arguments
+        and returns their sum as a float
+    """
     return (a + b)
